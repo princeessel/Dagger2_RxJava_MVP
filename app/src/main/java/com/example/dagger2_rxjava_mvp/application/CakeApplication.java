59618127efe6b1211bd2080656600f1ef30ffc75
princeessel <43561228+princeessel@users.noreply.github.com>
2019-01-22 21:26:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dagger2_rxjava_mvp/application/CakeApplication.java
@@ -0,0 +1,17 @@
+package com.example.dagger2_rxjava_mvp.application;
+
+import android.app.Application;
+
+public class CakeApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
